95b3408058da7b4a6884b0651704c585e3d5d0a6
TonyKuo <tony_kuo@asus.com>
2024-01-26 09:59:35 +0800

    detech dark/light from window.matchMedia

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 import './style.scss'
 
-const themes = ['rog', 'tuf', 'light'];
+// get query string for href and check if it has isModelApp param
+const queryString = new URLSearchParams(window.location.search)
+const isModelApp = queryString.get('isModelApp') === 'true';
+const currentTheme = queryString.get('theme');
 
-// generate buttons for each theme
-// click handler will change the class on the body
-const buttons = themes.map(theme => {
-    const button = document.createElement('button');
-    button.innerText = theme;
-    button.addEventListener('click', () => {
-        document.body.className = theme;
+document.body.className = currentTheme;
+
+const h1 = document.createElement('h1');
+
+document.body.append(h1);
+
+if (!isModelApp) {
+    const themes = ['rog', 'tuf', 'light'];
+    const buttons = themes.map(theme => {
+        const button = document.createElement('button');
+        button.innerText = theme;
+        button.addEventListener('click', () => {
+            document.body.className = theme;
+        });
+        return button;
     });
-    return button;
-});
+    h1.innerText = 'Switch From UWP'
+    document.body.append(...buttons);
+} else {
+    h1.innerText = 'Switch From System'
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+        console.log(e.matches ? 'dark' : 'light')
 
-document.body.append(...buttons);
+        const newColorScheme = e.matches ? currentTheme : 'light';
+        document.body.className = newColorScheme;
+    });
+}
+
